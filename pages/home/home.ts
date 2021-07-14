d649f09c7d1cac43c08b7d1f96daa08f800470fc
KevinTuco <kevin.tuco101325@my.lsgh.edu.ph>
2021-07-14 11:09:36 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -10,73 +10,29 @@ export class HomePage {
   numMinion: number;
   minionDam: number;
   AC: number;
+  rollTemp: number;
+  roll: number;
   hordeHit: number;
+  battleType: string;
   fightResult: string;
+  minionDice: number;
+  minionDNum: number;
 
   minionHorde() {
-    this.bmi = this.weight / (this.height * this.height);
-    this.bmi = parseFloat(this.bmi.toFixed(2));
-
-    if (this.bmi < 18.5){
-      this.bmiMessage = "Underweight";
-    } else if (this.bmi > 18.5 && this.bmi <25){
-      this.bmiMessage = "Normal";
-    } else if (this.bmi >25 && this.bmi < 30){
-      this.bmiMessage = "Overweight";
-    } else{
-      this.bmiMessage = "Obese";
-    }
-  }
-  
-   calculateIWC() {
-    this.subtract = 5*12;
-    this.feetConvert = parseInt(this.feetHeight)*12;
-    switch(this.gender){
-      case "m":
-        if ((parseInt(this.feetHeight)<5)){
-          this.tooLow = "The calculations are only accurate for those who are at least 5 ft tall.";
-        }else if ((parseInt(this.feetHeight)>7)){
-          this.tooLow = "Are you sure? The tallest person in the world, Robert Wadlow, is 8 feet and 11.1 inches";
-        }else{
-            this.calcNum =parseInt(this.inchesHeight) + (this.feetConvert - this.subtract) ;
-            //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
-            this.resultMiller = 56.2 + (1.41 * this.calcNum);
-            this.resultMiller = parseFloat(this.resultMiller.toFixed(2));
-            // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet
-            this.resultRobinson = 52 + (1.9 * this.calcNum);
-            this.resultRobinson = parseFloat(this.resultRobinson.toFixed(2));
-            //Devine formula: 50.0 kg + 2.3 kg per every inch over 5 feet
-            this.resultDevine = 50.0 + (2.3 * this.calcNum);
-            this.resultDevine = parseFloat(this.resultDevine.toFixed(2));
-            // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
-            this.resultHamwi = 48.0 + (2.7 * this.calcNum);
-            this.resultHamwi = parseFloat(this.resultHamwi.toFixed(2));
-            this.tooLow = ``;
+        this.minionDam = 0;
+        this.hordeHit = this.numMinion;
+        while(this.numMinion > 0){
+          this.roll = Math.floor(Math.random() * 20) + 1;
+          if (this.roll == this.AC || this.roll > this.AC){
+            while(this.minionDNum > 0){
+              this.rollTemp = Math.floor(Math.random() * this.minionDice) + 1;
+              this.minionDam += this.rollTemp;
+              this.minionDNum --;
+            }
+          }else if(this.roll < this.AC){
+            this.hordeHit --;
+          }
         }
-        break;
-      case "f":
-        if ((parseInt(this.feetHeight)<5)){
-          this.tooLow = "The calculations are only accurate for those who are at least 5 ft tall.";
-        }else if ((parseInt(this.feetHeight)>7)){
-          this.tooLow = "Are you sure? The tallest person in the world, Robert Wadlow, is 8 feet and 11.1 inches";
-        }else{
-          this.calcNum =parseInt(this.inchesHeight) + (this.feetConvert - this.subtract) ;
-          //Miller formula: 53.1 kg + 1.36 kg per every inch over 5 feet
-          this.resultMiller = 53.1 + (1.36 * this.calcNum);
-          this.resultMiller = parseFloat(this.resultMiller.toFixed(2));
-          // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet
-          this.resultRobinson = 49.0 + (1.7 * this.calcNum);
-          this.resultRobinson = parseFloat(this.resultRobinson.toFixed(2));
-          //Devine formula: 45.5 kg + 2.3 kg per every inch over 5 feet
-          this.resultDevine = 45.5 + (2.3 * this.calcNum);
-          this.resultDevine = parseFloat(this.resultDevine.toFixed(2));
-          //Hamwi formula: 45.5 kg + 2.2 kg per every inch over 5 feet
-          this.resultHamwi = 45.5 + (2.2 * this.calcNum);
-          this.resultHamwi = parseFloat(this.resultHamwi.toFixed(2));
-          this.tooLow = ``;
-        }
-        break;
-    }
   }
   
   constructor(public navCtrl: NavController) {
